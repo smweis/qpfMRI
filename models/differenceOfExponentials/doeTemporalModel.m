5725c3309f808fdd5d7df6f1b7f3c3b5e1bba817
gkaguirre <gkaguirre@me.com>
2019-04-30 13:16:26 -0400

    Need fminsearch to avoid local minima
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- models/differenceOfExponentials/doeTemporalModel.m
+++ models/differenceOfExponentials/doeTemporalModel.m
@@ -81,8 +81,8 @@ end
 
 % Find the specific S values that produce the desired beta amplitude
 F = @(w,S1,S2) (S1.*exp(-k1.*w)) - (S2.*exp(-k2.*w));
-myObj = @(S1) beta - ( max(F(freqDomain,S1,S1./Sr)) - min(F(freqDomain,S1,S1./Sr)) );
-S1 = fzero(myObj,1);
+myObj = @(S1) abs(beta - ( max(F(freqDomain,S1,S1./Sr)) - min(F(freqDomain,S1,S1./Sr)) ));
+S1 = fminsearch(myObj,1);
 S2 = S1/Sr;
 
 % Calculate the y values
