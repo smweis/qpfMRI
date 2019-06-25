c5e9dcffe380ac9240fcc3c63a16fe97b3fe1e98
nfuser <nfuser@huntsman-ve507-0544.apn.wlan.upenn.edu>
2019-06-25 09:26:25 -0400

    cleaning and editing readme

---
+++ realTime/runRealtimeQuestTFE.m
@@ -0,0 +1,95 @@
+
+
+%% Initialize Q+
+
+modelType = 'watson';
+modelParameters = struct;
+modelParameters.tau = 0.5:0.5:2;	% time constant of the center filter (in msecs)
+modelParameters.kappa = 0.5:0.5:2;	% multiplier of the time-constant for the surround
+modelParameters.zeta = 0:0.5:2;	% multiplier of the amplitude of the surround
+modelParameters.beta = 0.5:0.5:2; % multiplier that maps watson 0-1 to BOLD % bins
+modelParameters.sigma = 0:0.5:2;	% width of the BOLD fMRI noise against the 0-1 y vals
+
+
+headroom = .1;
+[myQpParams, questDataAtStart] = realTime_Init(modelType,modelParameters,'headroom',headroom);
+
+% Initialize questDataUpdated
+questDataUpdated = questDataAtStart;
+
+%{
+modelType = 'doe';
+modelParameters = struct;
+modelParameters.Sr = 0.899:0.025:1.099;
+modelParameters.k1 = 0.01:0.005:0.03;
+modelParameters.k2 = 0.5:0.05:1;
+modelParameters.beta = 0.5:0.1:2; % Amplitude of the scaled response; should converge to unity
+modelParameters.sigma = 0:0.1:0.5;	% Standard deviation of the scaled (0-1) noise
+
+headroom = .1;
+[myQpParams, questDataAtStart] = realTime_Init(modelType,modelParameters,'headroom',headroom);
+
+%}
+
+
+% initialize a main data vector
+mainDataVectorized = [];
+
+
+% CHANGE ME TO THE SCANNER PATH OR WHEREVER ACTUAL STIMULI ARE BEING SAVED! 
+stimDataPath = fullfile(subjectPath,strcat('actualStimuli',run,'.txt'));
+
+
+
+
+
+
+
+
+
+% If not at scanner, just load the stimuli we already presented. 
+if ~atScanner
+    stimData = load(fullfile('/Users','nfuser','Documents','rtQuest','TOME_3021_optimal',horzcat('TOME_3021_run',run),horzcat('stimDataRun',run,'.mat')));
+    stimulusVec = stimData.params.stimFreq;
+end
+
+
+
+
+mainDataPath = fullfile(subjectPath,'processed',strcat('run',run),strcat('mainData',run));
+
+i = 0;
+j = 1;
+
+
+% Wait for the main data file to exist. 
+while ~exist(mainDataPath,'file')
+    continue
+    pause(.01);
+end
+    
+
+while i < 10000000000
+    i = i + 1;
+    
+
+    load(mainDataPath,'mainData');
+   
+    
+    % Turn main data into a vector
+    mainDataVectorized(end+1:end+length(mainData(j).roiSignal)) = mainData(j).roiSignal;
+
+    questDataUpdated = realTime_Update(mainDataVectorized,...
+                   stimDataPath,...
+                   questDataAtStart, questDataUpdated, myQpParams, modelType,...
+                    'headroom',headroom,...
+                   'simulationStimVec',stimulusVec);
+    
+    % CHANGE ME TO THE PATH ON THE SCANNER
+    % Write new stimulus suggestion for use with play_flash                           
+    writeNewStimSuggestion(qpQuery(questDataUpdated),fullfile(subjectPath,'stimLog'));
+
+    j = j + 1;
+
+end
+
