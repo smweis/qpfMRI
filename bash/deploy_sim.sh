111d70fc089878293c15682037a2235fa337a266
Weisberg <stevenweisberg@ufl.edu>
2020-05-18 11:01:58 -0400

    adding example bash scripts

--- bash/deploy_sim.sh
+++ bash/deploy_sim.sh
@@ -9,9 +9,7 @@
 #SBATCH --output=sim_test_%a.log   # Standard output and error log
 
 
-# Call this script with the following positional arguments:
-# 1. Whether to use Q+ or not.
-# 2. Name of the individual output file ('doe_[name].csv')
+# See details of /code/qpfMRI/main/compiledSimulate.m for how to call this.
 
 sleep 1
 
@@ -21,10 +19,31 @@ ml matlab
 
 export MCR_CACHE_ROOT=$SCRATCH
 
+d=`date +%m-%d-%Y`
 
+cd /ufrc/stevenweisberg/stevenweisberg/qpfMRIResults/compiled.$d
 
-cd /ufrc/stevenweisberg/stevenweisberg/compiledDoe/
-
-./run_doeSimulate.sh /apps/matlab/mcr/2019b/v97 $4 $5 $6 1 .4 800 12 $1 $2 $3
-
-date
+./run_simulate.sh /apps/matlab/mcr/2019b/v97 \
+doeTemporalModel \# model name
+param1Lower .899 \
+param1Interval .025 \
+param1Upper 1.099 \
+param2Lower .01 \
+param2Interval .04 \
+param2Upper .4 \
+param3Lower .01 \
+param3Interval .04 \
+param3Upper .4 \
+param4Lower .8 \
+param4Interval .1 \
+param4Upper .1.4 \
+param5Lower .3 \
+param5Interval .2 \
+param5Upper 1.0 \
+param1Simulated 1.004 \
+param2Simulated .016 \
+param3Simulated .118 \
+param4Simulated 1.0 \
+param5Simulated .1 \ 
+qpPres $1 \ 
+outNum $2
--- bash/sim_wrapper.sh
+++ bash/sim_wrapper.sh
@@ -2,8 +2,8 @@
 
 for RUN in {0001..0050}
 do
-  sbatch deploy_sim.sh 0 "yes_maxBOLD_fix_false_"$RUN $RANDOM 1.0 0.1 0.2
+  sbatch deploy_sim.sh false $RUN
   sleep .1
-  sbatch deploy_sim.sh 1 "yes_maxBOLD_fix_true_"$RUN $RANDOM 1.0 0.1 0.2
+  sbatch deploy_sim.sh true $RUN
   sleep .1
 done
